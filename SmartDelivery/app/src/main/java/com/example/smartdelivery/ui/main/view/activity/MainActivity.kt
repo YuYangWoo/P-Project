e7cbec5d78d21641601396b7646dec6b03b1b605
YuYangWoo <yuyw0712@naver.com>
2021-12-08 17:45:08 +0900

    기본 레이아웃 구성

---
+++ SmartDelivery/app/src/main/java/com/example/smartdelivery/ui/main/view/activity/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.smartdelivery.ui.main.view.activity
+
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.codingassignment.ui.base.BaseActivity
+import com.example.smartdelivery.R
+import com.example.smartdelivery.databinding.ActivityMainBinding
+
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main){
+
+    // Fragment Attach
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment).navController
+    }
+
+    // Fragment appBar연결
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            setOf(R.id.mainFragment)
+        )
+    }
+
+    // Toolbar Set
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun init() {
+        super.init()
+        initToolbar()
+
+    }
+
+    // 뒤로가기 이벤트
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+}
---
+++ SmartDelivery/app/src/main/java/com/example/smartdelivery/ui/main/view/fragment/MainFragment.kt
@@ -0,0 +1,13 @@
+package com.example.smartdelivery.ui.main.view.fragment
+
+import com.example.codingassignment.ui.base.BaseFragment
+import com.example.smartdelivery.R
+import com.example.smartdelivery.databinding.FragmentMainBinding
+
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+
+    override fun init() {
+        super.init()
+        //TODO
+    }
+}

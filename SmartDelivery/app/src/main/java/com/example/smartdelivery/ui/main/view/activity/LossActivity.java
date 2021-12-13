49da0f31f6e6db076f0a6dde3b647dc0bf348782
hadora98kim <4082370@naver.com>
2021-12-13 14:18:00 +0900

    layout edit

--- SmartDelivery/app/src/main/java/com/example/smartdelivery/ui/main/view/activity/LossActivity.java
+++ SmartDelivery/app/src/main/java/com/example/smartdelivery/ui/main/view/activity/LossActivity.java
@@ -1,15 +1,35 @@
 package com.example.smartdelivery.ui.main.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.smartdelivery.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LossActivity extends AppCompatActivity {
+    //분실조회
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loss);
+        setContentView(R.layout.activity_loss_none);
+        Intent intent=new Intent(this,MenuActivity.class);
+        ImageButton button=findViewById(R.id.btn_back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
+
+
+
+
